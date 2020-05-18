8370c23ab2645331caef00ad07dbad02291fbb54
Jangyusu <60203731+Jangyusu@users.noreply.github.com>
2020-05-18 19:10:57 +0900

    index js

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,33 +1,81 @@
 window.addEventListener("DOMContentLoaded", function () {
     //start
 
-    var visualBg = document.querySelector(".visual__bg"),
-        visualText = document.querySelectorAll(".visual__text_wrap"),
+    var visualCurrentBg = document.querySelector(".visual__bg"),
+        visualNextBg = document.querySelectorAll(".visual__bg")[1],
+        visualText = document.querySelectorAll(".visual__text"),
         visualLine = document.querySelector(".visual__line_bold"),
         visualLink = document.querySelector(".visual__text_link"),
-        headerMenu = document.querySelector(".header__menu");
+        headerMenu = document.querySelector(".header__menu"),
+        visualCurrent = document.querySelector(".visual__ind_current"),
+        visualTotal = document.querySelectorAll(".visual__text").length,
+        visualNext = document.querySelector(".visual__ind_down"),
+        visualPrev = document.querySelector(".visual__ind_up"),
+        visualIndex = 0,
+        visualTotalText = document.querySelector(".visual__ind_total"),
+        visualBln = true;
 
-    visualBg.style.backgroundImage = "url('asset/img/index/bg_01.jpg')"; //visual first bg
+    console.log(visualText);
+
+
+
+    visualCurrent.innerHTML = "0" + (visualIndex + 1); //visual 현재 슬라이드 입력
+    visualTotalText.innerHTML = "0" + visualTotal; //visual total 슬라이드 입력
 
     headerMenu.addEventListener("click", function () { //header menu on/off
         this.classList.toggle("active");
     })
 
+    var autoSlide = setInterval(function () {
+        visualNext.click();
+    }, 5000);
 
-    setInterval(function () { //visual Bg on
-        visualBg.classList.add("active");
-    }, 1);
+    visualNext.addEventListener("click", () => visualControl(1, visualTotal, 0)) //visual Next 버튼
+    visualPrev.addEventListener("click", () => visualControl(-1, -1, visualTotal - 1)) //visual Prev 버튼
 
-    setInterval(function () {
-        visualLine.classList.add("active"); //visual Line on
-        for (var i = 0; i < visualText.length; i++) { //visual Text on
-            visualText[i].classList.add("active");
+    visualCurrentBg.style.backgroundImage = "url('asset/img/index/bg_01.jpg')"; //visual current 배경   
+
+
+
+    function visualControl(calc, condition, reset) { //visual Next, Prev 실행
+        if (visualBln == true) { //중복실행 방지
+            visualBln = false;
+
+            visualLine.classList.remove("active"); //visual Line off
+            visualText[visualIndex].classList.add("active");
+
+            visualIndex = visualIndex + calc;
+            if (visualIndex == condition) { //visual Index 초기화
+                visualIndex = reset;
+            };
+            visualCurrent.innerHTML = "0" + (visualIndex + 1); //visual Index업데이트
+
+            if (calc == 1) { //visual Next 실행
+                slideUpDown("150%", "slideUp");
+            } else { //visual Prev 실행
+                slideUpDown("-50%", "slideDown");
+            }
+
+            function slideUpDown(top, className) {
+                visualNextBg.style.top = top; //visual Next 배경
+                visualNextBg.style.backgroundImage = "url('asset/img/index/bg_0" + (visualIndex + 1) + ".jpg')"; //visual Next 배경
+                visualCurrentBg.classList.add(className);
+                visualNextBg.classList.add(className);
+                setTimeout(function () {
+                    visualCurrentBg.style.backgroundImage = visualNextBg.style.backgroundImage; //visual Next 배경
+                    visualCurrentBg.classList.remove(className);
+                    visualNextBg.classList.remove(className);
+                    visualNextBg.style.top = "150%"; //visual Next 배경
+                }, 1800);
+            }
+
+            setTimeout(function () {
+                visualBln = true;
+
+                visualLine.classList.add("active"); //visual Line on
+            }, 1800);
         }
-
-        setInterval(function () { //view Project on
-            visualLink.classList.add("active");
-        }, 1500);
-    }, 500);
+    };
 
     //end
 })
