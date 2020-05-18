6479290ec919692412917211fe57afd7b3f0c076
Jangyusu <60203731+Jangyusu@users.noreply.github.com>
2020-05-18 12:02:33 +0900

    index css

--- asset/js/index.js
+++ asset/js/index.js
@@ -3,20 +3,30 @@ window.addEventListener("DOMContentLoaded", function () {
 
     var visualBg = document.querySelector(".visual__bg"),
         visualText = document.querySelectorAll(".visual__text_wrap"),
-        visualLine = document.querySelector(".visual__line_bold");
+        visualLine = document.querySelector(".visual__line_bold"),
+        visualLink = document.querySelector(".visual__text_link"),
+        headerMenu = document.querySelector(".header__menu");
 
-    visualBg.style.backgroundImage = "url('asset/img/index/bg_01.jpg')"; //visual 첫 bg
+    visualBg.style.backgroundImage = "url('asset/img/index/bg_01.jpg')"; //visual first bg
+
+    headerMenu.addEventListener("click", function () { //header menu on/off
+        this.classList.toggle("active");
+    })
 
 
-    setInterval(function () {
+    setInterval(function () { //visual Bg on
         visualBg.classList.add("active");
     }, 1);
 
     setInterval(function () {
-        visualLine.classList.add("active");
-        for (var i = 0; i < visualText.length; i++) {
+        visualLine.classList.add("active"); //visual Line on
+        for (var i = 0; i < visualText.length; i++) { //visual Text on
             visualText[i].classList.add("active");
         }
+
+        setInterval(function () { //view Project on
+            visualLink.classList.add("active");
+        }, 1500);
     }, 2000);
 
     //end
